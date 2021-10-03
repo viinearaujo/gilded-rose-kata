66be0647bc26ddc8f22c3e6fe7df396079cdae27
viaviinearaujo <you@example.com>
2021-10-03 18:36:07 -0300

    refactor gilded rose

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,41 +1,5 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
-
-    def __init__(self, items):
-        self.items = items
-
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
-
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -44,3 +8,47 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class GildedRose(object):
+    AGED_BRIE = 'Aged Brie'
+    BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert'
+    SULFURAS = 'Sulfuras, Hand of Ragnaros'
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            self.update_item_quality(item)
+
+    def update_item_quality(self, item):
+        if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES:
+                if item.quality > 0:
+                    if item.name != self.SULFURAS:
+                        item.quality = item.quality - 1
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+                if item.name == self.BACKSTAGE_PASSES:
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+        if item.name != self.SULFURAS:
+            item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if item.name != self.AGED_BRIE:
+                if item.name != self.BACKSTAGE_PASSES:
+                    if item.quality > 0:
+                        if item.name != self.SULFURAS:
+                            item.quality = item.quality - 1
+                else:
+                    item.quality = item.quality - item.quality
+            else:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+
+

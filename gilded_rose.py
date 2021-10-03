0d71ad058a5bda11a9c78afd75aec7b53c106b3d
viaviinearaujo <you@example.com>
2021-10-03 20:01:25 -0300

    refactor gilded rose

--- gilded_rose.py
+++ gilded_rose.py
@@ -25,24 +25,27 @@ class GildedRose(object):
 
     def update_item_quality(self, item):
         decrease_quality_value = -2 if item.name == self.CONJURED else -1
-        if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES:
-                if item.quality > 0:
-                    if item.name != self.SULFURAS:
-                        self._adjust_quality(item, decrease_quality_value)
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-                if item.name == self.BACKSTAGE_PASSES:
-                    if item.sell_in < 11:
-                        self._adjust_quality(item, 1)
-                    if item.sell_in < 6:
-                        self._adjust_quality(item, 1)
+        quality_should_drop = item.name != self.AGED_BRIE \
+                            and item.name != self.BACKSTAGE_PASSES \
+                            and item.name != self.SULFURAS
+        if quality_should_drop:
+            self._adjust_quality(item, decrease_quality_value)
+
+        if item.name == self.AGED_BRIE:
+            self._adjust_quality(item, 1)
+
+        if item.name == self.BACKSTAGE_PASSES:
+            self._adjust_quality(item, 1)
+            if item.sell_in < 11:
+                self._adjust_quality(item, 1)
+            if item.sell_in < 6:
+                self._adjust_quality(item, 1)
+
         if item.name != self.SULFURAS:
             item.sell_in = item.sell_in - 1
         if item.sell_in < 0:
             if item.name != self.AGED_BRIE:
                 if item.name != self.BACKSTAGE_PASSES:
-                    if item.quality > 0:
                         if item.name != self.SULFURAS:
                             self._adjust_quality(item, decrease_quality_value)
                 else:

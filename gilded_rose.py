0ab2a0d3fc628e0946c6eb4346b1b852da60d472
viaviinearaujo <you@example.com>
2021-10-07 11:05:52 -0300

    overall refactor

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,31 +1,29 @@
 # -*- coding: utf-8 -*-
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+from typing import List
+from item import Item
 
 
-class GildedRose(object):
+class GildedRose(List[Item]):
     AGED_BRIE = 'Aged Brie'
     BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert'
     SULFURAS = 'Sulfuras, Hand of Ragnaros'
     CONJURED = 'Conjured Mana Cake'
 
-    def __init__(self, items):
+    def __init__(self, items: List[Item]):
         self.items = items
 
     def update_quality(self):
+        """Updates the quality of every item in Gilded Rose's inventory."""
         for item in self.items:
             self.update_item_quality(item)
 
-    def update_item_quality(self, item):
+    def update_item_quality(self, item: Item):
+        """Updates the quality of a specific item in Gilded Rose's
+        inventory."""
         after_sellin = item.sell_in < 1
-        decrease_quality_value = self._set_decrease_quality_value(item, after_sellin)
+        decrease_quality_value = self._set_decrease_quality_value(item, 
+                                                                after_sellin
+        )
         quality_should_drop = item.name != self.AGED_BRIE \
                             and item.name != self.BACKSTAGE_PASSES \
                             and item.name != self.SULFURAS
@@ -46,6 +44,8 @@ class GildedRose(object):
 
     def _handle_backstage_passes_quality(self, item: Item, 
             after_sellin: bool) -> None:
+        """Adjusts the quality of the Backstage Passes item in Gilded Rose's
+        inventory."""
         self._adjust_quality(item, 1)
         if item.sell_in < 11:
             self._adjust_quality(item, 1)
@@ -56,17 +56,16 @@ class GildedRose(object):
 
     def _set_decrease_quality_value(self, item: Item, 
             after_sellin: bool) -> int:
+        """Defines the amount an item should have its quality drop."""
         decrease_quality_value = -2 if item.name == self.CONJURED else -1
         if after_sellin:
             decrease_quality_value *= 2
         return decrease_quality_value
     
     def _adjust_quality(self, item: Item, value: int) -> None:
+        """Adjusts item quality if applicable."""
         new_quality = item.quality + value
         should_adjust = True if new_quality >= 0 \
                         and new_quality <= 50 else False
         if should_adjust:
             item.quality = new_quality
-
-
-
